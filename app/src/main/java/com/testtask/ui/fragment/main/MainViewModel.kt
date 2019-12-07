293ed42a98bbe35d6ff5032e302626209a6e8bbf
Simon Bashkirov <sb@smartsatu.com>
2019-12-07 22:32:51 +0300

    Bind data from viewmodel to main fragment

--- app/src/main/java/com/testtask/ui/fragment/main/MainViewModel.kt
+++ app/src/main/java/com/testtask/ui/fragment/main/MainViewModel.kt
@@ -1,7 +1,49 @@
 package com.testtask.ui.fragment.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import com.testtask.ui.model.ProfileShort
+import com.testtask.ui.model.TransactionItem
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private val profile = MutableLiveData<ProfileShort>().apply {
+        value = ProfileShort(
+            "sfasfasdfasfsadfsafadfasfsadfsafsadfasdfsdsfdafafsafdasfasdfafasdfasfafadfsdfafafasfa",
+            "ivanov",
+            "cull@gmil.com"
+        )
+    }
+
+    val firstName = Transformations.map(profile) { it.firstName }
+
+    val lastName = Transformations.map(profile) { it.lastName }
+
+    val email = Transformations.map(profile) { it.email }
+
+    private val _transactions = MutableLiveData<List<TransactionItem>>()
+    val transactions = _transactions as LiveData<List<TransactionItem>>
+
+    private val _total = MutableLiveData<String>()
+    val total = _total as LiveData<String>
+
+    fun startButtonClicked() {
+        Log.d("TAG", "start button clicked")
+    }
+
+    fun stopButtonClicked() {
+        Log.d("TAG", "stop button clicked")
+    }
+
+    fun clearButtonClicked() {
+        Log.d("TAG", "clear button clicked")
+    }
+
+    fun signOutButtonClicked() {
+        Log.d("TAG", "sign out button clicked")
+    }
+
 }
