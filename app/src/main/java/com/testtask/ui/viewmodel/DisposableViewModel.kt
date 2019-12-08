0c822ee8c758f73cf135a88ec4875f373ceee186
Simon Bashkirov <sb@smartsatu.com>
2019-12-08 17:32:56 +0300

    Added disposable viewmodel

---
+++ app/src/main/java/com/testtask/ui/viewmodel/DisposableViewModel.kt
@@ -0,0 +1,19 @@
+package com.testtask.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+
+class DisposableViewModel : ViewModel() {
+
+    private val disposables = CompositeDisposable()
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
+
+    protected fun addDisposable(disposable: Disposable) {
+        disposables.add(disposable)
+    }
+}
