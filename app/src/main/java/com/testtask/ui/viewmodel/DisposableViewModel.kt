d0fcc58043fd5df2bb0ab2bd6303f2ab16d59fba
Simon Bashkirov <sb@smartsatu.com>
2019-12-08 17:48:30 +0300

    Made disposable viewmodel abstract class

--- app/src/main/java/com/testtask/ui/viewmodel/DisposableViewModel.kt
+++ app/src/main/java/com/testtask/ui/viewmodel/DisposableViewModel.kt
@@ -4,7 +4,7 @@ import androidx.lifecycle.ViewModel
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 
-class DisposableViewModel : ViewModel() {
+abstract class DisposableViewModel : ViewModel() {
 
     private val disposables = CompositeDisposable()
 
