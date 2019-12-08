09ceda8eefed19d0b0e1574e8ae1a046694c7907
Simon Bashkirov <sb@smartsatu.com>
2019-12-08 17:16:47 +0300

    Added domainModule injection

--- app/src/main/java/com/testtask/injection/modules.kt
+++ app/src/main/java/com/testtask/injection/modules.kt
@@ -1,5 +1,9 @@
 package com.testtask.injection
 
+import com.testtask.data.repository.AuthRepositoryImpl
+import com.testtask.data.repository.UserRepsitoryImpl
+import com.testtask.domain.repository.AuthRepository
+import com.testtask.domain.repository.UserRepository
 import com.testtask.ui.activity.MainActivityViewModel
 import com.testtask.ui.fragment.auth.AuthViewModel
 import com.testtask.ui.fragment.main.MainViewModel
@@ -16,5 +20,13 @@ val uiModule = module {
     viewModel { MainViewModel() }
 
     viewModel { MainActivityViewModel() }
+}
+
+val domainModule = module {
+
+    single<UserRepository> { UserRepsitoryImpl() }
+
+    single<AuthRepository> { AuthRepositoryImpl() }
+
 }
 
