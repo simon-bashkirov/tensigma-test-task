02188bf87886a21897dcb8f61a9c34d550555383
Simon Bashkirov <sb@smartsatu.com>
2019-12-08 17:50:06 +0300

    Usecase fix

--- app/src/main/java/com/testtask/domain/interactor/auth/ObserveAuthStateUseCase.kt
+++ app/src/main/java/com/testtask/domain/interactor/auth/ObserveAuthStateUseCase.kt
@@ -1,13 +1,15 @@
 package com.testtask.domain.interactor.auth
 
 import com.testtask.domain.interactor.FlowableInteractor
+import com.testtask.domain.interactor.auth.ObserveAuthStateUseCase.NoParams
 import com.testtask.domain.repository.AuthRepository
 import com.testtask.domain.state.AuthState
 
 class ObserveAuthStateUseCase(private val authRepository: AuthRepository) :
-    FlowableInteractor<Nothing, AuthState> {
+    FlowableInteractor<NoParams, AuthState> {
 
-    override fun execute(params: Nothing) =
+    override fun execute(params: NoParams) =
         authRepository.getAuthState()
 
+    object NoParams
 }
--- app/src/main/java/com/testtask/domain/interactor/user/ObserveMyFirstProfileUseCase.kt
+++ app/src/main/java/com/testtask/domain/interactor/user/ObserveMyFirstProfileUseCase.kt
@@ -1,12 +1,15 @@
 package com.testtask.domain.interactor.user
 
 import com.testtask.domain.interactor.FlowableInteractor
+import com.testtask.domain.interactor.user.ObserveMyFirstProfileUseCase.NoParams
 import com.testtask.domain.model.user.Profile
 import com.testtask.domain.repository.UserRepository
 
 class ObserveMyFirstProfileUseCase(private val userRepository: UserRepository) :
-    FlowableInteractor<Nothing, Profile> {
+    FlowableInteractor<NoParams, Profile> {
 
-    override fun execute(params: Nothing) =
+    override fun execute(params: NoParams) =
         userRepository.getCurrentUserInfo().map { it.profiles.first() }
+
+    object NoParams
 }
