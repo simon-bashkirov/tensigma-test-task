3fd4327b4e7e9348a603fc39ce9c04ce180a4568
Simon Bashkirov <sb@smartsatu.com>
2019-12-09 19:23:21 +0300

    Added transaction repository

---
+++ app/src/main/java/com/testtask/domain/repository/TransactionRepository.kt
@@ -0,0 +1,16 @@
+package com.testtask.domain.repository
+
+import com.testtask.domain.model.transaction.Transaction
+import io.reactivex.Completable
+import io.reactivex.Flowable
+
+interface TransactionRepository {
+
+    fun startTransactionStream(): Completable
+
+    fun stopTransactionStream(): Completable
+
+    fun clearTransactionCache(): Completable
+
+    fun getTransactionStream(): Flowable<Transaction>
+}
